8a71070fc1cbd7a395f9e546d8010e83842fb802
shadowsymphony12 <shadowsymphony8@gmail.com>
2025-07-13 22:02:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,9 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('webcachex-v1').then(cache => {
-      return cache.addAll(['/', '/index.html', '/app.js']);
-    })
-  );
+const CACHE = 'pro-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/style.css', '/app.js', '/db.js', '/manifest.json'])));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
