eb563e0b69da165af2fe25ba70d4ad10e9287d59
shadowsymphony12 <shadowsymphony8@gmail.com>
2025-07-14 11:59:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,23 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('webcachex-pro').then(cache => {
-      return cache.addAll(['/', '/index.html', '/style.css', '/app.js']);
+    caches.open('webcachex-static').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/db.js',
+        '/manifest.json',
+        '/icon.png'
+      ]);
     })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
     })
   );
 });
