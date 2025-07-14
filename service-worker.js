9af13c2499f354dc98c3ac7544bc6ef5b1e6715c
shadowsymphony12 <shadowsymphony8@gmail.com>
2025-07-14 12:29:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,35 @@
+const CACHE_NAME = 'webcachex-v2';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/db.js',
+  '/manifest.json',
+  '/icon.png'
+];
+
+// Cache on install
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('webcachex-static').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js',
-        '/db.js',
-        '/manifest.json',
-        '/icon.png'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
+// Clean up old cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+// Serve from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(resp => {
