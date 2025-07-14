34a411f1e6863ba25d684dca0e1d3c5e8fbb08a7
shadowsymphony12 <shadowsymphony8@gmail.com>
2025-07-14 15:30:56 +0530

    Update db.js

--- db.js
+++ db.js
@@ -23,11 +23,11 @@ async function savePage(url, content) {
 async function getAllPages() {
   const db = await getDB();
   const tx = db.transaction(storeName, "readonly");
+  const store = tx.objectStore(storeName);
   return new Promise((resolve, reject) => {
-    const store = tx.objectStore(storeName);
-    const req = store.getAll();
-    req.onsuccess = () => resolve(req.result);
-    req.onerror = () => reject("Failed to get data");
+    const request = store.getAll();
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject("Failed to fetch pages");
   });
 }
 
