8cae1441c26205dfeddb1548ad34d2db671a20b1
shadowsymphony12 <shadowsymphony8@gmail.com>
2025-07-14 13:04:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,67 +1,51 @@
-const DB_NAME = "WebCacheXProDB";
-const DB_VERSION = 1;
-const STORE_NAME = "cache";
+// Simple IndexedDB wrapper for caching
 
-let db;
+const dbName = "WebCacheXDB";
+const storeName = "pages";
 
-function openDB() {
+function getDB() {
   return new Promise((resolve, reject) => {
-    const request = indexedDB.open(DB_NAME, DB_VERSION);
+    const request = indexedDB.open(dbName, 1);
+
     request.onerror = () => reject("Failed to open DB");
-    request.onsuccess = () => {
-      db = request.result;
-      resolve(db);
-    };
-    request.onupgradeneeded = (event) => {
-      db = event.target.result;
-      if (!db.objectStoreNames.contains(STORE_NAME)) {
-        db.createObjectStore(STORE_NAME, { keyPath: "url" });
-      }
+
+    request.onsuccess = () => resolve(request.result);
+
+    request.onupgradeneeded = (e) => {
+      const db = e.target.result;
+      db.createObjectStore(storeName, { keyPath: "url" });
     };
   });
 }
 
 async function savePage(url, content) {
-  const db = await openDB();
-  const tx = db.transaction(STORE_NAME, "readwrite");
-  const store = tx.objectStore(STORE_NAME);
-  store.put({ url, content, timestamp: new Date() });
+  const db = await getDB();
+  const tx = db.transaction(storeName, "readwrite");
+  tx.objectStore(storeName).put({ url, content });
   return tx.complete;
 }
 
-async function getPage(url) {
-  const db = await openDB();
-  return new Promise((resolve) => {
-    const tx = db.transaction(STORE_NAME, "readonly");
-    const store = tx.objectStore(STORE_NAME);
-    const request = store.get(url);
-    request.onsuccess = () => resolve(request.result?.content || null);
-    request.onerror = () => resolve(null);
-  });
-}
-
 async function getAllPages() {
-  const db = await openDB();
-  return new Promise((resolve) => {
-    const tx = db.transaction(STORE_NAME, "readonly");
-    const store = tx.objectStore(STORE_NAME);
+  const db = await getDB();
+  const tx = db.transaction(storeName, "readonly");
+  const store = tx.objectStore(storeName);
+  return new Promise((resolve, reject) => {
     const request = store.getAll();
     request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject("Failed to fetch pages");
   });
 }
 
 async function deletePage(url) {
-  const db = await openDB();
-  const tx = db.transaction(STORE_NAME, "readwrite");
-  const store = tx.objectStore(STORE_NAME);
-  store.delete(url);
+  const db = await getDB();
+  const tx = db.transaction(storeName, "readwrite");
+  tx.objectStore(storeName).delete(url);
   return tx.complete;
 }
 
 async function clearAllStoredPages() {
-  const db = await openDB();
-  const tx = db.transaction(STORE_NAME, "readwrite");
-  const store = tx.objectStore(STORE_NAME);
-  store.clear();
+  const db = await getDB();
+  const tx = db.transaction(storeName, "readwrite");
+  tx.objectStore(storeName).clear();
   return tx.complete;
 }
